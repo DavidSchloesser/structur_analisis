0f6adf628678dcec198c05e34e1903839206e55d
Schloesser <david.schloesser@zeiss.com>
2022-05-19 15:10:00 +0200

    Added new styling

---
+++ script.js
@@ -0,0 +1,529 @@
+var blue = [
+    "digitaler",
+    "ordnung",
+    "funktion",
+    "funktionen",
+    "zeit",
+    "faktor",
+    "faktoren",
+    "mechanik",
+    "zertifikat",
+    "zertifikate",
+    "planung",
+    "zielgruppe",
+    "vorbereitung",
+    "vorbereitungen",
+    "information",
+    "informationen",
+    "symmetrie",
+    "test",
+    "kosten",
+    "organigramm",
+    "koordination",
+    "denken",
+    "rechnen",
+    "rechnung",
+    "maße",
+    "analyse",
+    "analysieren",
+    "analysiere",
+    "analysiert",
+    "rentabilität",
+    "rentabel",
+    "prüfung",
+    "nutzen",
+    "berechnung",
+    "berechnungen",
+    "statistik",
+    "statistiken",
+    "organisation",
+    "index",
+    "auflistung",
+    "checkliste",
+    "plan",
+    "planung",
+    "konzept",
+    "konzeption",
+    "innovation",
+    "innovationen",
+    "möglichkeit",
+    "möglichkeiten",
+    "zukunft",
+    "weitblick",
+    "studie",
+    "ergebnis",
+    "fazit",
+    "bedeutung",
+    "gedanken",
+    "sinnhaftigkeit",
+    "listen",
+    "tabellen",
+    "chart",
+    "vortrag",
+    "qualität",
+    "preis",
+    "leistung",
+    "kompetenz",
+    "pünktlichkeit",
+    "prospekt",
+    "prüfung",
+    "distanz",
+    "legitimation",
+    "beweis",
+    "beratung",
+    "vorsorge",
+    "erkenntnis",
+    "beweise",
+    "unabhängigkeit",
+    "perfektion",
+    "reflexion",
+    "vernunft",
+    "überzeugung",
+    "anerkennung",
+    "zukunft",
+    "individualität",
+    "weitblick",
+    "daten",
+    "zahlen",
+    "präzise",
+    "rational",
+    "praktisch",
+    "edel",
+    "fachgerecht",
+    "funktional",
+    "leise",
+    "pragmatisch",
+    "vollendet",
+    "qualitativ",
+    "ergiebig",
+    "wirtschaftlich",
+    "energiesparend",
+    "sparsam",
+    "zuverlässig",
+    "mechanisch",
+    "individuell",
+    "automatisch",
+    "selbstreinigend",
+    "rein",
+    "anatomisch",
+    "korrekt",
+    "vornehmen",
+    "körpergerecht",
+    "elegant",
+    "komplett",
+    "symmetrisch",
+    "langlebig",
+    "variabel",
+    "übersichtlich",
+    "fortschrittlich",
+    "messbar",
+    "sinnvoll",
+    "planbar",
+    "vernünftig",
+    "zukunftsorietiert",
+    "sachlich",
+    "genau",
+    "überzeugend",
+    "durchdacht",
+    "ordentlich",
+    "passgenau",
+    "exakt",
+    "strukturiert",
+    "kompetent",
+    "fraglich",
+    "perfekt",
+    "prozesse",
+    "prozess",
+    "digitale",
+    "individuelle",
+    "präzision",
+    "fachkundig",
+    "fachkundige",
+    "professionell",
+    "professionelle",
+    "high-tech"
+];
+
+var red = [
+    "einzigartiges",
+    "einkaufserlebniss",
+    "beeindruckendes",
+    "begeistern",
+    "augenblick",
+    "gewinner",
+    "sieger",
+    "stärke",
+    "wettbewerb",
+    "autorität",
+    "ziel",
+    "macht",
+    "kraft",
+    "chef",
+    "führer",
+    "dominanz",
+    "orginal",
+    "kampf",
+    "umsatz",
+    "angebot",
+    "wettkampf",
+    "power",
+    "aktion",
+    "praxis",
+    "dynamik",
+    "entscheidung",
+    "richtung",
+    "erfolg",
+    "zuwachs",
+    "expansion",
+    "mittelpunk",
+    "verdrängung",
+    "sport",
+    "bewegung",
+    "figur",
+    "klarheit",
+    "einfachheit",
+    "chance",
+    "offenheit",
+    "umsetzbarkeit",
+    "bekanntheit",
+    "exklusivität",
+    "kampfgeist",
+    "meinung",
+    "standfestigkeit",
+    "innovationsfreudigkeit",
+    "beste",
+    "neuste",
+    "schönste",
+    "vorsprung",
+    "überlegenheit",
+    "anerkennung",
+    "belohnung",
+    "aktivität",
+    "leistung",
+    "reiz",
+    "erlebnis",
+    "spontatität",
+    "marke",
+    "aktion",
+    "schönheit",
+    "robustheit",
+    "fakten",
+    "späktakulär",
+    "ohnegleichen",
+    "wirkungsvoll",
+    "orginell",
+    "jugendlich",
+    "frisch",
+    "ungewöhnlich",
+    "überdimensional",
+    "überragend",
+    "eindrucksvoll",
+    "grandios",
+    "großartig",
+    "imposant",
+    "herrlich",
+    "attraktiv",
+    "exklusiv",
+    "exklusives",
+    "attraktives",
+    "herliches",
+    "grandioses",
+    "eindrucksvolles",
+    "imposantes",
+    "überragendes",
+    "ungewöhnliches",
+    "frisches",
+    "orginelles",
+    "wirkungsvolles",
+    "großartiges",
+    "preiswert",
+    "sehenswert",
+    "ausgefallen",
+    "preiswertes",
+    "sehenswertes",
+    "ausgefallenes",
+    "prachvoll",
+    "erlesen",
+    "repräsentativ",
+    "kolossal",
+    "aufwendig",
+    "exotisch",
+    "neu",
+    "erstklassig",
+    "sensationell",
+    "wahnsinnig",
+    "beispiellos",
+    "interessant",
+    "unbeschreiblich",
+    "praktisch",
+    "einmalig",
+    "praktisch",
+    "groß",
+    "fit",
+    "belastbar",
+    "sportlich",
+    "mitreißend",
+    "super",
+    "geil",
+    "verfügbar",
+    "kurz",
+    "knapp",
+    "direkt",
+    "schnell",
+    "widerstandsfähig",
+    "sofort",
+    "cool",
+    "hart",
+    "einzigartig",
+    "bombastisch",
+    "luxus",
+    "lifestyle",
+    "leistet",
+    "unvergleichlich",
+    "geschwindigkeit",
+    "vorsprung"
+];
+
+var green = [
+    "kundenbedürfnisse",
+    "service",
+    "komfort",
+    "wunderbar",
+    "heiterkeit",
+    "seele",
+    "gelassenheit",
+    "erfahrung",
+    "vergangenheit",
+    "werte",
+    "tradition",
+    "freunde",
+    "familie",
+    "gemeinschaft",
+    "menschen",
+    "kinder",
+    "personen",
+    "mensch",
+    "person",
+    "kind",
+    "freund",
+    "team",
+    "umwelt",
+    "freude",
+    "historie",
+    "vertrauen",
+    "spaß",
+    "locker",
+    "lockerheit",
+    "natürlichkeit",
+    "ausgewogenheit",
+    "toleranz",
+    "zusammenhalt",
+    "ruhe",
+    "geborgenheit",
+    "atmosphäre",
+    "gefühl",
+    "verbindung",
+    "fantasie",
+    "nähe",
+    "gespür",
+    "beliebtheit",
+    "vertrauen",
+    "sympathie",
+    "harmonie",
+    "stimmung",
+    "liebe",
+    "nettigkeit",
+    "aufmerksamkeit",
+    "wir",
+    "geselligkeit",
+    "gemütlichkeit",
+    "sicherheit",
+    "bestätigung",
+    "bequemlichkeit",
+    "überleben",
+    "genuss",
+    "wohlbefinden",
+    "gewohnheit",
+    "freundlichkeit",
+    "gruppe",
+    "sozial",
+    "behaglich",
+    "geschmackvoll",
+    "halbar",
+    "leger",
+    "bequem",
+    "risikolos",
+    "lustig",
+    "aufgelockert",
+    "dekorativ",
+    "gemütlich",
+    "pflegeleicht",
+    "heilsam",
+    "wohltuend",
+    "glaubhaft",
+    "unkompliziert",
+    "arbeitserleichternd",
+    "wohlgefällig",
+    "ungezwungen",
+    "beliebt",
+    "erotisch",
+    "sinnlich",
+    "verführerisch",
+    "konstant",
+    "sicher",
+    "ausgeglichen",
+    "harmonisch",
+    "beruhigend",
+    "familiengerecht",
+    "farbenfroh",
+    "stimmungsvoll",
+    "nett",
+    "beständig",
+    "einfach",
+    "mühelos",
+    "freundlich",
+    "bewährt",
+    "zuvorkommend",
+    "traditionell",
+    "rücksichtsvoll",
+    "spaßig",
+    "schön",
+    "angenehm",
+    "aufmerksam",
+    "beängstigend"
+];
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    analyze();
+})
+
+//replaces from each instance of the words in the array
+function analyze() {
+    var user = document.getElementById("userInput").value;
+    user = user.toLowerCase();
+    var analyze = user;
+    var sentGreen = 0;
+    var sentBlue = 0;
+    var sentRed = 0;
+    var sentiment = 0;
+    //this accounts for only matching whole words enclosing the pattern with word boundaries
+    //it also adds an random string that should never be seen naturally for counting purposes
+
+    for (var i = 0; i < green.length; i++) {
+        var greenReg = RegExp(
+            "\\b" + green[i].replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + "\\b",
+            "g"
+        );
+        analyze = analyze.replace(
+            greenReg,
+            "<span class='green'>" + green[i] + "</span>"
+        );
+        if (analyze.match(greenReg)) {
+            ++sentGreen;
+        }
+    }
+
+    for (var i = 0; i < blue.length; i++) {
+        var blueReg = RegExp(
+            "\\b" + blue[i].replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + "\\b",
+            "g"
+        );
+        analyze = analyze.replace(
+            blueReg,
+            "<span class='blue'>" + blue[i] + "</span>"
+        );
+        if (analyze.match(blueReg)) {
+            ++sentBlue;
+        }
+    }
+    for (var i = 0; i < red.length; i++) {
+        var redReg = RegExp(
+            "\\b" + red[i].replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + "\\b",
+            "g"
+        );
+        analyze = analyze.replace(
+            redReg,
+            "<span class='red'>" + red[i] + "</span>"
+        );
+        if (analyze.match(redReg)) {
+            ++sentRed;
+        }
+    }
+
+    //write the edited input to the output side
+    var out = document.getElementById("outputArea");
+    out.innerHTML = analyze;
+
+    //add up all of the number of classes of lexicon words and multiply them by their sentiment score
+    /*
+    sentPos5 = out.querySelectorAll(".pos5");
+    sentiment = sentiment + sentPos5.length * 5;
+    sentPos4 = out.querySelectorAll(".pos4");
+    sentiment = sentiment + sentPos4.length * 4;
+    sentPos3 = out.querySelectorAll(".pos3");
+    sentiment = sentiment + sentPos3.length * 3;
+    sentPos2 = out.querySelectorAll(".pos2");
+    sentiment = sentiment + sentPos2.length * 2;
+    sentPos1 = out.querySelectorAll(".pos1");
+    sentiment = sentiment + sentPos1.length * 1;
+    sentNeg5 = out.querySelectorAll(".neg5");
+    sentiment = sentiment + sentNeg5.length * -5;
+    sentNeg4 = out.querySelectorAll(".neg4");
+    sentiment = sentiment + sentNeg4.length * -4;
+    sentNeg3 = out.querySelectorAll(".neg3");
+    sentiment = sentiment + sentNeg3.length * -3;
+    sentNeg2 = out.querySelectorAll(".neg2");
+    sentiment = sentiment + sentNeg2.length * -2;
+    sentNeg1 = out.querySelectorAll(".neg1");
+    sentiment = sentiment + sentNeg1.length * -1;
+  
+    //get the total number of words in the lexicon included in the text
+    var totalLex =
+      sentNeg1.length +
+      sentNeg1.length +
+      sentNeg2.length +
+      sentNeg3.length +
+      sentNeg4.length +
+      sentNeg5.length +
+      sentPos1.length +
+      sentPos2.length +
+      sentPos3.length +
+      sentPos4.length +
+      sentPos5.length;
+  */
+
+    sentiment = sentBlue * +4 + sentRed * +3 + sentGreen * 1;
+
+    var totalLex = sentBlue + sentRed + sentGreen;
+    //dont let the avg divide by 0
+    if (totalLex === 0) {
+        totalLex = 1;
+    }
+    //get the average score to normalize between -5 and 5
+    var sentimentAvg = Math.round((sentiment / totalLex) * 100) / 100;
+    //write the score to the screen
+    var sent = (document.getElementById(
+        "sentimentScoreGreen"
+    ).innerHTML = sentGreen);
+    var sent = (document.getElementById("sentimentScoreRed").innerHTML = sentRed);
+    var sent = (document.getElementById(
+        "sentimentScoreBlue"
+    ).innerHTML = sentBlue);
+    //write an emoticon to the screen
+    var sentFace;
+    if (sentimentAvg <= -3) {
+        sentFace = "ðŸ¤¬";
+    } else if (sentimentAvg > -3 && sentimentAvg <= -2) {
+        sentFace = "â˜¹ï¸";
+    } else if (sentimentAvg > -2 && sentimentAvg <= -1) {
+        sentFace = "ðŸ™";
+    } else if (sentimentAvg > -1 && sentimentAvg <= 1) {
+        sentFace = "ðŸ˜";
+    } else if (sentimentAvg > 1 && sentimentAvg <= 2) {
+        sentFace = "ðŸ™‚";
+    } else if (sentimentAvg > 2 && sentimentAvg <= 3) {
+        sentFace = "ðŸ˜Š";
+    } else if (sentimentAvg > 3) {
+        sentFace = "ðŸ˜„";
+    }
+}
